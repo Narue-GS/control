2901e138eff2cac7946ef9e6596f53af6934aabb
Naruê Gonçalves Santos <naruegit@gmail.com>
2024-05-19 10:47:05 -0300

    feat: create crud's visual funcionalities

---
+++ app/(backend)/api/(modules)/invoice/services.ts
@@ -0,0 +1,51 @@
+export interface IInvoice {
+  id:number,
+  clientName:string,
+  clientContact:string,
+  service:string,
+  value:number
+}
+
+const mock:IInvoice[] = [
+  {
+    id:0,
+    clientName:"Impact Hub",
+    clientContact:"financeiro@impacthub.net",
+    service:"Host de comunidades",
+    value:1500
+  },
+  {
+    id:1,
+    clientName:"Impact Hub",
+    clientContact:"financeiro@impacthub.net",
+    service:"Host de eventos - 1 de Maio de 2024",
+    value:165
+  },
+  {
+    id:2,
+    clientName:"Impact Hub",
+    clientContact:"financeiro@impacthub.net",
+    service:"Host de eventos - 28 de Abril de 2024",
+    value:165
+  },
+ 
+]
+
+
+export async function READ(): Promise<IInvoice[] | []> {
+    try {
+      const data = await (mock).map((i) => (
+        {
+          id:i.id,
+          clientName:i.clientName,
+          clientContact:i.clientContact,
+          service:i.service,
+          value:i.value
+        }))
+      return data
+    }
+    catch(err) {
+      console.log(err)
+      return[]
+    }
+}
